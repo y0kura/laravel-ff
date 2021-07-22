826a7ef8986a335756a52de013317ab187ad46ab
ToshiyukiYokura <80732830+ToshiyukiYokura@users.noreply.github.com>
2021-07-22 12:36:32 +0900

    オーバーライド

---
+++ classes/Brave.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Brave extends Human
+{
+    //プロパティ
+    const MAX_HITPOINT = 120;
+    public $hitPoint=self::MAX_HITPOINT;
+    public $attackPoint=30;
+    public function doAttack($enemy)
+    {
+        //乱数の発生
+        if(rand(1,3)===1){
+            //スキル発動
+            echo "『{$this->name}』のスキルが発動した！\n";
+            echo "『ぜんりょくぎり』！！\n";
+            echo $enemy->name."に".$this->attackPoint * 1.5."のダメージ！\n";
+            $enemy->tookDamage($this->attackPoint * 1.5);
+        }else{
+            parent::doAttack($enemy);
+        }
+        return true;
+    }
+}
