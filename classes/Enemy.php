4e3ae7632841a210b9be96a668f1c80c30cee070
ToshiyukiYokura <80732830+ToshiyukiYokura@users.noreply.github.com>
2021-07-22 11:56:24 +0900

    Enemyクラス実装

---
+++ classes/Enemy.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Enemy
+{
+    // プロパティ
+    const MAX_HITPOINT =50;
+    public $name;
+    public $hitPoint=50;
+    public $attackPoint=10;
+
+    // メソッド
+    public function doAttack($human)
+    {
+        echo"『{$this->name}』の攻撃!\n";
+        echo"【{$human->name}】に{$this->attackPoint}のダメージ!\n";
+        $human->tookDamage($this->attackPoint);
+    }
+
+    public function tookDamage($damage)
+    {
+        $this->hitPoint -= $damage;
+        // HPが0未満にならないための処理
+        if($this->hitPoint < 0){
+            $this->hitPoint = 0;
+        }
+    }
+}
