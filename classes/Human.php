d38d69619adf4d081293210d6caae33cafcd2436
ToshiyukiYokura <80732830+ToshiyukiYokura@users.noreply.github.com>
2021-07-22 11:34:43 +0900

    Humanクラス実装

---
+++ classes/Human.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Human
+{
+    //    プロパティ
+    const MAX_HITPOINT = 100; // 最大HPの定義 定数
+    public $name; // キャラ名
+    public $hitPoint=100; // 現在のHP
+    public $attackPoint=20; // 攻撃力
+
+    // メソッド
+    public function doAttack($enemy)
+    {
+        echo "『{$this->name}』の攻撃!\n";
+        echo "【{$enemy->name}】に{$this->attackPoint}のダメージ!\n";
+        $enemy->tookDamage($this->attackPoint);
+    }
+
+    public function tookDamage($damage)
+    {
+        $this->hitPoint -= $damage;
+        // HPが0未満にならないための処理
+        if($this->hitPoint < 0){
+            $this->hitPoint = 0;
+        }
+    }
+}
